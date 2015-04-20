c2c73dd2a97e672ed4a99fa17a5cc29fd3965a0c
Anderson Marques <Andersonoanjo18@gmail.com>
2015-04-20 19:56:08 -0300

    buscando register id

--- app/src/main/java/br/com/acception/arautoapp/ArautoMainActivity.java
+++ app/src/main/java/br/com/acception/arautoapp/ArautoMainActivity.java
@@ -1,10 +1,15 @@
 package br.com.acception.arautoapp;
 
+import android.app.Activity;
+import android.app.ProgressDialog;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.android.volley.Request.*;
@@ -13,34 +18,62 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.gms.gcm.GoogleCloudMessaging;
 
 import org.json.JSONObject;
 
+import java.io.IOException;
+import java.util.HashMap;
 import java.util.Map;
 
+import br.com.acception.arautoapp.util.OperacaoAsyncTask;
 
-public class ArautoMainActivity extends ActionBarActivity {
+
+public class ArautoMainActivity extends Activity {
     Map<String, String> params;
     private RequestQueue rq;
     private String url;
+    GoogleCloudMessaging gcm;
+    String regid;
+    ProgressDialog progress;
+    TextView ed;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_arauto_main);
-        url = "http://0.0.0.0:8080/gcm/criaGcm";
+        ed = (TextView) findViewById(R.id.textView);
+        url = "http://192.168.0.106:8080/gcm/criaGcm";
         rq = Volley.newRequestQueue(ArautoMainActivity.this);
     }
 
+    /*
+    Método que vai registar o Android no google gcm server
+     */
+    public void registrarAndroidNoGoogle(View view){
+        OperacaoAsyncTask op = new OperacaoAsyncTask(this);
+        Void vo = null;
+        op.execute(vo);
+    }
+
+    public void MostrarResultado(String regid){
+        this.regid = regid;
+        this.ed.setText(regid);
+    }
 
     //chamadas volley
     public void callByJsonObjectRequest(View view){
-        this.params = null;
+        this.params = new HashMap<String, String>();
+        this.params.put("client_id", "");
+        this.params.put("client_secret", "");
+        this.params.put("grant_type", "");
+        this.params.put("scopes", "");
+
         CustomJsonObjectRequest cjor = new CustomJsonObjectRequest(Method.POST, this.url, this.params,
                 new Response.Listener<JSONObject>() {
             @Override
             public void onResponse(JSONObject response) {
-                log.i("Volley Teste", "Sucesso: "+response);
+                Log.i("Volley Teste", "Sucesso: " + response);
             }
         }, new Response.ErrorListener() {
             @Override
---
+++ app/src/main/java/br/com/acception/arautoapp/util/OperacaoAsyncTask.java
@@ -0,0 +1,70 @@
+package br.com.acception.arautoapp.util;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.google.android.gms.gcm.GoogleCloudMessaging;
+
+import org.json.JSONException;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+import br.com.acception.arautoapp.ArautoMainActivity;
+
+public class OperacaoAsyncTask extends AsyncTask<Void, Void, Void> {
+	Context contexto;
+    GoogleCloudMessaging gcm;
+    String regid;
+    String NUMERO_PROJETO = "247352349874"; //projeto que tem de ser criado no google
+	ProgressDialog progress;
+	
+	public OperacaoAsyncTask(Context context) {
+        this.contexto = context;
+	}
+	
+	
+	@Override
+    protected void onPreExecute()
+    {
+		progress = ProgressDialog.show(contexto, "Pegando Regid no google...", "Aguarde um pouquinho XD...");
+    }				
+	
+	@Override
+	protected Void doInBackground(Void... params) {
+		// TODO Auto-generated method stub
+		Map<String, String> parameters = new HashMap<String, String>();
+
+        String msg = "";
+        while (true) {
+            try {
+                if (gcm == null) {
+                    gcm = GoogleCloudMessaging.getInstance(contexto.getApplicationContext());
+                }
+                regid = gcm.register(NUMERO_PROJETO);
+                msg = "Disposistivo Registrado, Id registro = " + regid;
+                Log.i("RegID", msg);
+                break;
+            } catch (IOException ex) {
+                msg = "Erro " + ex.getMessage();
+            }
+        }
+
+
+		//parameters.put("RegId", regid);
+        //parameters.put("id_projeto", "736300024f653c669ef602eeb295e0c594e258e6ac257111f9f797edf03122cc9b9267c253eadfe38b094a546f8f13faac4305baedf628e36a0497caaf3448b867339d0946503b2c00ffbc1658df83076926057a263db451456c7f94f6b382a6348188b8");
+
+		return null;
+	}
+	
+	@Override
+    protected void onPostExecute(Void res)
+    {
+		ArautoMainActivity ope = (ArautoMainActivity) contexto;
+		progress.dismiss();
+		ope.MostrarResultado(this.regid);
+    }
+}
